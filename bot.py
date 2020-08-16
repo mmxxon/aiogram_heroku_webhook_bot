de3410ca5325ce68cc571ab0cdb55d83aae75c31
Maksym Movchan <xondev@vivaldi.net>
2020-08-16 17:46:10 +0300

    bot

--- bot.py
+++ bot.py
@@ -2,48 +2,40 @@ import os
 import logging
 
 from aiogram import Bot, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
-from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils import executor
 from aiogram.utils.executor import start_webhook
+from aiogram.dispatcher.webhook import get_new_configured_app
 
+PROJECT_NAME = os.environ["PROJ"]
 TOKEN = os.environ["TOKEN"]
 
-WEBHOOK_HOST = os.environ["URL"]
-WEBHOOK_PATH = '/webhook/' + TOKEN
+WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
+WEBHOOK_PATH = "/webhook/" + TOKEN
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-WEBAPP_HOST = '0.0.0.0'
+WEBAPP_HOST = "0.0.0.0"
 WEBAPP_PORT = int(os.environ.get("PORT", 5000))
-
-logging.basicConfig(level=logging.INFO)
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
+logging.basicConfig(level=logging.INFO)
+
 
 @dp.message_handler(commands='start')
 async def start_handler(message: types.Message):
-    await bot.send_message(message.chat.id, text='hi')
+    await bot.send_message(message.chat.id, text="hi")
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    # Regular request
-    # await bot.send_message(message.chat.id, message.text)
-
-    # or reply INTO webhook
-    return SendMessage(message.chat.id, message.text)
-
-
-async def on_startup(dp):
+async def on_startup(app):
+    await bot.delete_webhook()
     await bot.set_webhook(WEBHOOK_URL)
-    # insert code here to run it after start
 
 
 async def on_shutdown(dp):
-    logging.warning('Shutting down..')
+    logging.warning("Shutting down..")
 
     # insert code here to run it before shutdown
 
@@ -54,16 +46,19 @@ async def on_shutdown(dp):
     await dp.storage.close()
     await dp.storage.wait_closed()
 
-    logging.warning('Bye!')
+    logging.warning("Bye!")
 
 
-if __name__ == '__main__':
-    start_webhook(
-        dp,
-        WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT
-    )
+if __name__ == "__main__":
+    if "HEROKU" in list(os.environ.keys):
+        start_webhook(
+            dispatcher=dp,
+            webhook_path=WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT
+        )
+    else:
+        executor.start_polling(dp)
