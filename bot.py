b85578f943f28511c153f711381ae0187f17cac3
Maksym Movchan <xondev@vivaldi.net>
2020-08-16 17:09:42 +0300

    Python version up

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import Bot, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
-from aiogram.dispatcher.webhook import SendMessage  
+from aiogram.dispatcher.webhook import SendMessage
 from aiogram.utils.executor import start_webhook
 
 TOKEN = os.environ["TOKEN"]
